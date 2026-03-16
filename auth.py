a401158df6df0b019a6ab56a77ce5ec471923246
renata-droid <renata@tftreinamentosdigitais.com>
2026-03-16 13:50:44 -0300

    Add files via upload

--- auth.py
+++ auth.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import requests
 
 CLIENT_ID = st.secrets["CLIENT_ID"]
 CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
 REDIRECT_URI = st.secrets["REDIRECT_URI"]
+
+def renovar_token():
+
+    url = "https://api.mercadolibre.com/oauth/token"
+
+    payload = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    }
+
+    response = requests.post(url, data=payload)
+    data = response.json()
+
+    return data["access_token"]
